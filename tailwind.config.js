0c827f31d67d068f30fb07b0e3b023f3093d530d
Sébastien <vandaele.seba@gmail.com>
2025-04-03 10:06:53 +0200

    add shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,81 +1,129 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    darkMode: ['class'],
+    content: ['./src/index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-      extend: {
-          fontSize: {
-              '2xs': '.625rem',
-              '3xs': '.5rem',
-          },
-          colors: {
-              primary: {
-                  DEFAULT: '#3490dc',
-                  dark: '#2779bd',
-              },
-              secondary: '#ffed4a',
-              danger: '#e3342f',
-          },  
-          scale: {
-              101: '1.01',
-              102: '1.02',
-          },
-          aspectRatio: {
-              'A4': '210/297',
-          },
-          gridTemplateColumns: {
-              '13': 'repeat(13, minmax(0, 1fr))',
-              '14': 'repeat(14, minmax(0, 1fr))',
-              '15': 'repeat(15, minmax(0, 1fr))',
-          },
-          spacing: {
-              '600px': '600px',
-              '80vh': '80vh',
-              '1/2': '50%',
-          },
-          margin: {
-              '18': '4.5rem',
-          },
-          zIndex: {
-              'auto': 'auto',
-              '0': '0',
-              '10': '10',
-              '20': '20',
-              '30': '30',
-              '40': '40',
-              '50': '50',
-              '60': '60',
-              '70': '70',
-              '80': '80',
-              '90': '90',
-              '100': '100',
-              '200': '200',
-          },
-          boxShadow: {
-              'custom-light': '0 2px 5px rgba(0, 0, 0, 0.1)',
-              'custom-dark': '0 4px 10px rgba(0, 0, 0, 0.25)',
-          },
-          keyframes: {
-              bounce: {
-                  '0%, 100%': { transform: 'translateY(0)' },
-                  '50%': { transform: 'translateY(-10px)' },
-              },
-              fadeIn: {
-                  '0%': { opacity: '0' },
-                  '100%': { opacity: '1' },
-              },
-          },
-          animation: {
-              bounce: 'bounce 1s infinite',
-              fadeIn: 'fadeIn 0.5s ease-out',
-          },
-          inset: {
-              '18': '4.5rem',
-          }
-      },
+  	extend: {
+  		fontSize: {
+  			'2xs': '.625rem',
+  			'3xs': '.5rem'
+  		},
+  		colors: {
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				dark: '#2779bd',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			danger: '#e3342f',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		scale: {
+  			'101': '1.01',
+  			'102': '1.02'
+  		},
+  		aspectRatio: {
+  			A4: '210/297'
+  		},
+  		gridTemplateColumns: {
+  			'13': 'repeat(13, minmax(0, 1fr))',
+  			'14': 'repeat(14, minmax(0, 1fr))',
+  			'15': 'repeat(15, minmax(0, 1fr))'
+  		},
+  		spacing: {
+  			'600px': '600px',
+  			'80vh': '80vh',
+  			'1/2': '50%'
+  		},
+  		margin: {
+  			'18': '4.5rem'
+  		},
+  		zIndex: {
+  			'0': '0',
+  			'10': '10',
+  			'20': '20',
+  			'30': '30',
+  			'40': '40',
+  			'50': '50',
+  			'60': '60',
+  			'70': '70',
+  			'80': '80',
+  			'90': '90',
+  			'100': '100',
+  			'200': '200',
+  			auto: 'auto'
+  		},
+  		boxShadow: {
+  			'custom-light': '0 2px 5px rgba(0, 0, 0, 0.1)',
+  			'custom-dark': '0 4px 10px rgba(0, 0, 0, 0.25)'
+  		},
+  		keyframes: {
+  			bounce: {
+  				'0%, 100%': {
+  					transform: 'translateY(0)'
+  				},
+  				'50%': {
+  					transform: 'translateY(-10px)'
+  				}
+  			},
+  			fadeIn: {
+  				'0%': {
+  					opacity: '0'
+  				},
+  				'100%': {
+  					opacity: '1'
+  				}
+  			}
+  		},
+  		animation: {
+  			bounce: 'bounce 1s infinite',
+  			fadeIn: 'fadeIn 0.5s ease-out'
+  		},
+  		inset: {
+  			'18': '4.5rem'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
   plugins: [
-      // require('tailwind-scrollbar-hide'),
-      // require('@tailwindcss/typography'),
-      // require('@tailwindcss/forms'),
-  ],
+      require("tailwindcss-animate")
+],
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   build: {
-    outDir: '../Api/Api/wwwroot',
+    outDir: "../Api/Api/wwwroot",
   },
   server: {
-      port: 3000,
-      open: true,
-      host: true
+    port: 3000,
+    open: true,
+    host: true,
   },
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
